864a051e2653c814f94c6195e5d653e91d562282
Demo <demowebv1@gmail.com>
2022-06-20 23:35:43 +0700

    update unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('should be contains Chào mừng', () => {
+test('should be contains Bản đồ', () => {
   render(<App />);
-  const headerElement = screen.getByText(/Chào mừng/i);
+  const headerElement = screen.getByText(/Bản đồ/i);
   expect(headerElement).toBeInTheDocument();
 });
